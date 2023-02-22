153a36f3109a90a58e84355f21fe085e8af89cb5
Patrick Goral <patgoral@gmail.com>
2023-02-22 16:37:44 -0500

    updated

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(db, {
 
 const app = express()
 
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://127.0.0.1:5505` }))
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:3000` }))
 
 app.use(express.json())
 app.use(requestLogger)
