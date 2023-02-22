830cc8cc9dc008c36a81b881375276e67bfbba0d
Patrick Goral <patgoral@gmail.com>
2023-02-22 16:12:48 -0500

    deploy

--- server.js
+++ server.js
@@ -20,13 +20,12 @@ mongoose.connect(db, {
 
 const app = express()
 
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://127.0.0.1:5505` }))
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:3000` }))
 
 app.use(express.json())
 app.use(requestLogger)
 
 app.use(ticketRoutes)
-app.use('/seed', ticketSeed)
 app.use(partRoutes)
 app.use(userRoutes)
 
